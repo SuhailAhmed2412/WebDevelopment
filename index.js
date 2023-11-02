0d47b3d785455b9abd9a55e79ace25c6ee34cd22
SuhailAhmed2412 <suhail.ahmed2412@outlook.com>
2023-11-02 12:41:04 -0500

    Updated Photos

---
+++ index.js
@@ -0,0 +1,11 @@
+let img1= document.getElementById("img-1");
+let img2= document.getElementById("img-2");
+let img3= document.getElementById("img-3");
+let img4= document.getElementById("img-4");
+
+setInterval(
+    function(){
+        for(let i=0; i>=0; i++) {
+            
+        }
+    },1000)
