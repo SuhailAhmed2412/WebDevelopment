6e523c8e20f0bafc9e6ae03b36eb719296c5d2ab
suhailahmed2412 <suhail.ahmed2412@outlook.com>
2023-11-04 17:20:12 +0530

    4-Nov-23

--- index.js
+++ index.js
@@ -1,11 +1,65 @@
-let img1= document.getElementById("img-1");
-let img2= document.getElementById("img-2");
-let img3= document.getElementById("img-3");
-let img4= document.getElementById("img-4");
+let img1 = document.getElementById('img-1');
+let img2 = document.getElementById('img-2');
+let img3 = document.getElementById('img-3');
+let img4 = document.getElementById('img-4');
+let awwalprice = document.getElementById('awwal-price');
+let aalaprice = document.getElementById('aala-price');
+let azeemprice = document.getElementById('azeem-price');
+
+let i=1;
 
 setInterval(
-    function(){
-        for(let i=0; i>=0; i++) {
-            
+    function() {
+        if (i%2 === 0) {
+            img1.style.display = "flex";
+            img2.style.display = "none";
+            img3.style.display = "none";
+            img4.style.display = "none";
+        } else if (i%3 === 0) {
+            img1.style.display = "none";
+            img2.style.display = "flex";
+            img3.style.display = "none";
+            img4.style.display = "none";
+        } else if (i%4 === 0) {
+            img1.style.display = "none";
+            img2.style.display = "none";
+            img3.style.display = "flex";
+            img4.style.display = "none";
+        } else {
+            img1.style.display = "none";
+            img2.style.display = "none";
+            img3.style.display = "none";
+            img4.style.display = "flex";
+        } i++;
+    },
+1000)
+
+setInterval(
+    function () {
+        if(awwalprice.style.display=== "none") {
+            awwalprice.style.display="block";
+        } else {
+            awwalprice.style.display="none";
         }
-    },1000)
+    }, 1000
+)
+
+setInterval(
+    function () {
+        if(aalaprice.style.display=== "none") {
+            aalaprice.style.display="block";
+        } else {
+            aalaprice.style.display="none";
+        }
+    }, 1000
+)
+
+setInterval(
+    function () {
+        if(azeemprice.style.display=== "none") {
+            azeemprice.style.display="block";
+        } else {
+            azeemprice.style.display="none";
+        }
+    }, 1000
+)
