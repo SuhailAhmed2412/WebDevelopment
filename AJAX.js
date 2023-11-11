3eef37907dd81e340a6973ba4f79791f6ea702c7
suhailahmed2412 <suhail.ahmed2412@outlook.com>
2023-11-11 21:08:25 +0530

    Get

--- AJAX.js
+++ AJAX.js
@@ -1,11 +1,21 @@
 var data= null;
 
 $('#button').click(function() {
-    $('#myDiv').load('AJA.txt',data,function(response, status, xhr){
-        if(status == 'success') {
-            alert('All Okay');
+    // $('#myDiv').load('https://imdb-api.projects.thetuhin.com/title/tt6522580',function(response, status, xhr){
+    //     if(status == 200) {
+    //         alert('All Okay');
+    //     } else if (status == 'error') {
+    //     alert('Error: ' + xhr.status+' '+xhr.statusText);
+    //     }
+    // });
+    $.get('https://imdb-api.projects.thetuhin.com/title/tt15354916', function(response, status, xhr){
+        if(xhr.status == 200) {
+            console.log('All Okay');
+            console.log(response);
+            $('#title').html(response.title); 
+            $('#img').attr('src',response.image); 
         } else if (status == 'error') {
-        alert('Error: ' + xhr.status+' '+xhr.statusText);
-        }
-    });
+            console.log('Error: ' + xhr.status+' '+xhr.statusText);
+        }          
+    })
 })
